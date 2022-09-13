009b148ffd6f88e74a682313c310210e7883e797
thiago <nasciutti.thiago@gmail.com>
2022-09-13 15:44:33 -0400

    apiroutes.js

---
+++ routes/apiroutes.js
@@ -0,0 +1,32 @@
+//Requires
+const router = require("express").Router();
+const db = require("../db/db.json");
+const fs = require("fs");
+
+//GET Notes
+router.get("/notes", (req, res) => {
+ res.json(db);
+});
+
+//POST New Note
+router.post("/notes", (req, res) => {
+ db.push(req.body);
+ fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+  return err;
+ });
+ res.json(db);
+});
+
+//DELETE Note
+router.delete("/notes/:id", (req, res) => {
+    console.log(db);
+    console.log(req.params.id)
+    db.splice(req.params.id-1,1);
+    fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+       return err;
+   });
+    res.json(db);
+   });
+
+//Export Module
+module.exports = router;
