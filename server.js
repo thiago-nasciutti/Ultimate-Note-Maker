65d8ad24580d16a4e2cd6e8dc7c3f868406b6794
thiago <nasciutti.thiago@gmail.com>
2022-09-13 15:37:50 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+//Require Express
+const express = require("express");
+
+//Require Routes
+const apiroutes = require("./routes/apiroutes");
+const htmlroutes = require("./routes/htmlroutes");
+
+//PORT: 3001
+const PORT = process.env.PORT||3001;
+
+//Use
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use("/api",apiroutes);
+app.use("/",htmlroutes);
+
+//Listener
+app.listen(PORT,() => console.log(`listening on PORT ${PORT}`));
